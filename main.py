150492d67e4f77ba8c21a7d83fbe911d17c0239a
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2023-08-28 18:47:48 -0300

    game_updated

--- main.py
+++ main.py
@@ -33,13 +33,14 @@ while game_is_on:
         score.score_count()
 
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        score.game_over()
-        game_is_on = False
+         score.reset()
+         snake.reset()
     #Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+
+            score.reset()
+            snake.reset()
     #if head collides with any segment in the tail :
         #trigger game_over
 
--- score.py
+++ score.py
@@ -1,32 +1,36 @@
 from turtle import Turtle
+
 ALIGN = 'center'
 FONT = ('Courier', 20, 'normal')
 LOCATION = (0, 250)
+
+with open("data.txt", mode="r") as data_score:
+    SCORE = int(data_score.read())
 
 
 class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.color("white")
-        self.penup()
-        self.hideturtle()
         self.score_num = 0
+        self.penup()
+        self.high_score = SCORE
+        self.hideturtle()
         self.goto(LOCATION)
         self.update_score()
 
     def update_score(self):
-        self.write(arg=f"Score = {self.score_num}", align=ALIGN, font=FONT)
+        self.clear()
+        self.write(arg=f"Score = {self.score_num} High Score: {self.high_score}", align=ALIGN, font=FONT)
 
     def score_count(self):
         self.score_num += 1
-        self.clear()
         self.update_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg=f"GAME OVER ", align=ALIGN, font=FONT)
-
-
-
-
-
+    def reset(self):
+        if self.score_num > self.high_score:
+            self.high_score = self.score_num
+            with open("data.txt", mode="w") as data_input:
+                data_input.write(str(self.high_score))
+        self.score_num = 0
+        self.update_score()
